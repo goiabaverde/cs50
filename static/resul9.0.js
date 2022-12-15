add246a8cc52eefcfed4b0cb63f0c8e63b24296e
Álvaro Nogueira <alvaro.nogueira@equals.com.br>
2022-12-15 16:04:14 -0300

    Ajustes resultjs

--- static/resul9.0.js
+++ static/resul9.0.js
@@ -63,198 +63,94 @@ window.addEventListener('load', results)
     
 //}else if()
 
-function results()
-{
-//result
-if(isNaN(x1) && isNaN(x2))
-{
-    
-    except.innerHTML = (`There is not a graph for this quadratic equation`)
-    res.innerHTML = (`This is a complex root`)
-    if(a>0 && b>0 && c>0 )
-    {
-    sa.innerHTML=(`a is positive, the graph has a <strong>concavity up</strong>`)
-    sb.innerHTML=(`b is positive and a is positive, the vertex will be on quadrant 2 and the graph will cross the y axie <strong>increasing</strong>`)
-    sc.innerHTML=(`c is positive, the graph will cross the y axie <strong>above</strong> the point 0`)
+function calcQuadrant(xv, yv) {
+
+    if (xv > 0 && yv > 0) {
+        return 1
+    } else if (xv > 0 && yv < 0) {
+        return 4
+    } else if (xv < 0 && yv < 0) {
+        return 3
+    } else {
+        return 2
+    }
+
+}
+
+function results() {
+
+    var quadrant = calcQuadrant(Xv, Yv)
+
+    sa.innerHTML=(`a is ${a > 0 ? `positive`: `negative`}, the graph has a <strong>concavity ${a > 0 ? `up`: `down`}</strong>`)
+    sb.innerHTML=(`b is ${b > 0 ? `positive`: `negative`} and a is  ${a > 0 ? `positive`: `negative`}, the vertex will be on quadrant ${quadrant} and the graph will cross the y axis <strong>${b > 0 ? `increasing`: `negadecreasingtive`}</strong>`)
+    sc.innerHTML=(`c is ${c > 0 ? `positive`: `negative`}, the graph will cross the y axis <strong>${c > 0 ? `above`: `below`} below</strong> the point 0`)
     xv.innerHTML=(`The value of Xv is <strong> ${Xv}</strong>`)
     yv.innerHTML=(`The value of Yv is <strong>${Yv}</strong>`)
     expression.innerHTML = (`Your quadratic function will look like: <i>${a}x² + ${b}x + ${c}</i>`)
 
-    // a positivo b negativo c positivo
-    }else if(a>0 && b<0 && c>0){
-    sa.innerHTML=(`a is positive, the graph has a <i><strong>concavity up</strong></i>`)
-    sb.innerHTML=(`b is negative and a is positive, the vertex will be on the quadrant 1 and the graph will cross the y axie <i>decreasing</i>`)
-    sc.innerHTML=(`c is positive, the graph will cross the y axie <i><strong>above</strong></i> the point 0`)
-    xv.innerHTML=(`The value of Xv is <i><strong> ${Xv}</strong></i>`)
-    yv.innerHTML=(`The value of Yv is <i><strong>${Yv}</strong></i>`)
-    expression.innerHTML = (`Your quadratic function will look like: <i>${a}x² ${b}x + ${c}</i>`)
-
-    // a positive, b positive, c negative
-    }else if(a>0 && b > 0 && c < 0)
-    {
-    sa.innerHTML=(`a is positive, the graph has a <i><strong>concavity up</strong></i>`)
-    sb.innerHTML=(`b is positive and a is positive, the vertex will be on quadrant 3 and the graph will cross the axie y <i><strong>increasing</strong></i>`)
-    sc.innerHTML=(`c is negative, the graph will cross the y axis <i><strong>below</strong></i> the point 0`)
-    xv.innerHTML=(`The value of Xv is <i><strong> ${Xv}</strong></i>`)
-    yv.innerHTML=(`The value of Yv is <i><strong>${Yv}</strong></i>`)
-    expression.innerHTML = (`Your quadratic function will look like: <i>${a}x² + ${b}x ${c}</i>`)
+        //result
+    if(isNaN(x1) && isNaN(x2)) {
+        
+        except.innerHTML = (`There is not a graph for this quadratic equation`)
+        res.innerHTML = (`This is a complex root`)
     
+        return 0;
 
-    // a positive, b negative, c negative
-    }else if(a>0 && b < 0 && c < 0)
-    {
-    sa.innerHTML=(`a is positive, the graph has a <i><strong>concavity up</strong></i>`)
-    sb.innerHTML=(`b is negative and a is positive, the vertex will be on quadrant 3 and the graph will cross the axie y <i><strong>decreasing</strong></i>`)
-    sc.innerHTML=(`c is negative, the graph will cross the y axis <i><strong>below</strong></i> the point 0`)
-    xv.innerHTML=(`The value of Xv is <i><strong> ${Xv}</strong></i>`)
-    yv.innerHTML=(`The value of Yv is <i><strong>${Yv}</strong></i>`)
-    expression.innerHTML = (`Your quadratic function will look like: <i>${a}x² ${b}x ${c}</i>`)
-    
+    } else {
 
-    // a negative, b positive, c positive
-    }else if(a < 0 && b > 0 && c > 0)
-    {
-    sa.innerHTML=(`a is negative, the graph has a <i><strong>concavity down</strong></i>`)
-    sb.innerHTML=(`b is positive and a is negative, the vertex will be on quadrant 2 and the graph will cross the axie y <i><strong>incrising</strong></i>`)
-    sc.innerHTML=(`c is positive, the graph will cross the y axis <i><strong>above</strong></i> the point 0`)
-    xv.innerHTML=(`The value of Xv is <i><strong> ${Xv}</strong></i>`)
-    yv.innerHTML=(`The value of Yv is <i><strong>${Yv}</strong></i>`)
-    expression.innerHTML = (`Your quadratic function will look like: <i>${a}x² + ${b}x + ${c}</i>`)
-    
-
-    // a negative b negative c positive
-    }else if(a < 0 && b < 0 && c > 0)
-    {
-    sa.innerHTML=(`a is negative, the graph has a <i><strong>concavity down</strong></i>`)
-    sb.innerHTML=(`b is negative and a is negative, the vertex will be on quadrant 2 and the graph will cross the axie y <i><strong>decreasing</strong></i>`)
-    sc.innerHTML=(`c is positive, the graph will cross the y axis <i><strong>above</strong></i> the point 0`)
-    xv.innerHTML=(`The value of Xv is <i><strong>${Xv}</strong></i>`)
-    yv.innerHTML=(`The value of Yv is <i><strong>${Yv}</strong></i>`)
-    expression.innerHTML = (`Your quadratic function will look like: <i>${a}x² ${b}x + ${c}</i>`)
-    
-
-    // a negative, b positive, c negative
-    }else if(a < 0 && b > 0 && c < 0)
-    {
-    sa.innerHTML=(`a is negative, the graph has a <i><strong>concavity down</strong></i>`)
-    sb.innerHTML=(`b is positive and a is negative, the vertex will be on quadrant 4 and the graph will cross the axie y <i><strong>increasing</strong></i>`)
-    sc.innerHTML=(`c is negative, the graph will cross the y axis <i><strong>below</strong></i> the point 0`)
-    xv.innerHTML=(`The value of Xv is <i><strong> ${Xv}</strong></i>`)
-    yv.innerHTML=(`The value of Yv is <i><strong>${Yv}</strong></i>`)
-    expression.innerHTML = (`Your quadratic function will look like: <i>${a}x² + ${b}x ${c}</i>`)
-    
-
-    // a negative, b negative, c negative 
-    }else if(a < 0 && b < 0 && c < 0)
-    {
-    sa.innerHTML=(`a is negative, the graph has a <i><strong>concavity down</strong></i>`)
-    sb.innerHTML=(`b is negative and a is negative, the vertex will be on <i><strong>quadrant 3</strong></i> and the graph will cross the axie y <strong>decreasing</strong>`)
-    sc.innerHTML=(`c is negative, the graph will cross the y axis <i><strong>below</strong></i> the point 0`)
-    xv.innerHTML=(`The value of Xv is <i><strong> ${Xv}</strong></i>`)
-    yv.innerHTML=(`The value of Yv is <i><strong>${Yv}</strong></i>`)
-    expression.innerHTML = (`Your quadratic function will look like: <i>${a}x² ${b}x ${c}</i>`)
+        if(x1 < x2) {
+            let X1 = x1.toFixed(2)
+            let X2 = x2.toFixed(2)
+            res.innerHTML=(`x1 é <i><strong>${X1}</strong></i> e x2 é <i><strong>${X2}</strong></i>`) 
+        } 
+        else if(x1 > x2) {
+            let X1 = x1.toFixed(2)
+            let X2 = x2.toFixed(2)
+            res.innerHTML=(`x1 é <i><strong>${X2}</strong></i> e x2 é <i><strong>${X1}</strong></i>`)
+            
+        }   
     }
-    return 0;
-}else
-{if(x1 < x2)
-{
-    let X1 = x1.toFixed(2)
-    let X2 = x2.toFixed(2)
-    res.innerHTML=(`x1 é <i><strong>${X1}</strong></i> e x2 é <i><strong>${X2}</strong></i>`) 
-}else if(x1 > x2)
-{
-    let X1 = x1.toFixed(2)
-    let X2 = x2.toFixed(2)
-    res.innerHTML=(`x1 é <i><strong>${X2}</strong></i> e x2 é <i><strong>${X1}</strong></i>`)
+
+    // a positivo, b positivo c positivo
+    if(a>0 && b>0 && c>0 ) {
     
-}   
-}
+        i.innerHTML = ('<img src="../static/imagens/raiz negatica, a positivo, b positivo, c positivo.png" alt="graphic">')
 
-// a positivo, b positivo c positivo
- if(a>0 && b>0 && c>0 )
- {
-    sa.innerHTML=(`a is positive, the graph has a <i><strong>concavity up</strong></i>`)
-    sb.innerHTML=(`b is positive and a is positive, the vertex will be on quadrant 3 and the graph will cross the y axie <i><strong>increasing</strong></i>`)
-    sc.innerHTML=(`c is positive, the graph will cross the y axie <i><strong>above</strong></i> the point 0`)
-    xv.innerHTML=(`The value of Xv is <i><strong> ${Xv}</strong></i>`)
-    yv.innerHTML=(`The value of Yv is <i><strong>${Yv}</strong></i>`)
-    i.innerHTML = ('<img src="../static/imagens/raiz negatica, a positivo, b positivo, c positivo.png" alt="graphic">')
-    expression.innerHTML = (`Your quadratic function will look like: <i>${a}x² + ${b}x + ${c}</i>`)
+        // a positivo b negativo c positivo
+    } else if(a>0 && b<0 && c>0) {
+    
+        i.innerHTML = ('<img src="../static/imagens/a positivo, b negativo, c positivo.png" alt="graphic">')
 
-    // a positivo b negativo c positivo
- }else if(a>0 && b<0 && c>0){
-    sa.innerHTML=(`a is positive, the graph has a <i><strong>concavity up</strong></i>`)
-    sb.innerHTML=(`b is negative and a is positive, the vertex will be on the quadrant 4 and the graph will cross the y axie <i><strong> decreasing</strong></i>`)
-    sc.innerHTML=(`c is positive, the graph will cross the y axie <i><strong>above</strong></i> the point 0`)
-    xv.innerHTML=(`The value of Xv is <i><strong> ${Xv}</strong></i>`)
-    yv.innerHTML=(`The value of Yv is <i><strong>${Yv}</strong></i>`)
-    i.innerHTML = ('<img src="../static/imagens/a positivo, b negativo, c positivo.png" alt="graphic">')
-    expression.innerHTML = (`Your quadratic function will look like: <i>${a}x² ${b}x + ${c}</i>`)
+        // a positive, b positive, c negative
+    } else if(a>0 && b > 0 && c < 0) {
+    
+        i.innerHTML = ('<img src="../static/imagens/a positivo, b positivo, c negativo.png" alt="graph">')
 
-    // a positive, b positive, c negative
- }else if(a>0 && b > 0 && c < 0)
- {
-    sa.innerHTML=(`a is positive, the graph has a <strong>concavity up</strong>`)
-    sb.innerHTML=(`b is positive and a is positive, the vertex will be on quadrant 3 and the graph will cross the axie y <i><strong>increasing</strong></i>`)
-    sc.innerHTML=(`c is negative, the graph will cross the y axis <i><strong>below</strong></i> the point 0`)
-    xv.innerHTML=(`The value of Xv is <i><strong> ${Xv}</strong></i>`)
-    yv.innerHTML=(`The value of Yv is <i><strong>${Yv}</strong></i>`)
-    i.innerHTML = ('<img src="../static/imagens/a positivo, b positivo, c negativo.png" alt="graph">')
-    expression.innerHTML = (`Your quadratic function will look like: <i>${a}x² + ${b}x ${c}</i>`)
+        // a positive, b negative, c negative
+    } else if(a>0 && b < 0 && c < 0) {
+    
+        i.innerHTML = ('<img src="../static/imagens/a positivo, b negativo, c negativo.png" alt="graph">')
 
-    // a positive, b negative, c negative
- }else if(a>0 && b < 0 && c < 0)
- {
-    sa.innerHTML=(`a is positive, the graph has a <i><strong>concavity up</strong></i>`)
-    sb.innerHTML=(`b is negative and a is positive, the vertex will be on quadrant 3 and the graph will cross the axie y <i><strong>decreasing</strong></i>`)
-    sc.innerHTML=(`c is negative, the graph will cross the y axis <i><strong>below</strong></i> the point 0`)
-    xv.innerHTML=(`The value of Xv is <i><strong> ${Xv}</strong></i>`)
-    yv.innerHTML=(`The value of Yv is <i><strong>${Yv}</strong></i>`)
-    i.innerHTML = ('<img src="../static/imagens/a positivo, b negativo, c negativo.png" alt="graph">')
-    expression.innerHTML = (`Your quadratic function will look like: <i>${a}x² ${b}x ${c}</i>`)
+        // a negative, b positive, c positive
+    } else if(a < 0 && b > 0 && c > 0) {
+    
+        i.innerHTML = ('<img src="../static/imagens/a negativo, b positivo, c positivo.png" alt="graph">')
 
-    // a negative, b positive, c positive
- }else if(a < 0 && b > 0 && c > 0)
-{
-    sa.innerHTML=(`a is negative, the graph has a <i><strong>concavity down</strong></i>`)
-    sb.innerHTML=(`b is positive and a is negative, the vertex will be on quadrant 1 and the graph will cross the axie y <i><strong>incrising</strong></i>`)
-    sc.innerHTML=(`c is positive, the graph will cross the y axis <i><strong>above</strong></i> the point 0`)
-    xv.innerHTML=(`The value of Xv is <i><strong> ${Xv}</strong></i>`)
-    yv.innerHTML=(`The value of Yv is <i><strong>${Yv}</strong></i>`)
-    i.innerHTML = ('<img src="../static/imagens/a negativo, b positivo, c positivo.png" alt="graph">')
-    expression.innerHTML = (`Your quadratic function will look like: <i>${a}x² + ${b}x + ${c}</i>`)
+        // a negative b negative c positive
+    } else if(a < 0 && b < 0 && c > 0) {
+    
+        i.innerHTML = ('<img src="../static/imagens/a negative, b negative, c positive .png" alt="graph">')
 
-    // a negative b negative c positive
-}else if(a < 0 && b < 0 && c > 0)
-{
-    sa.innerHTML=(`a is negative, the graph has a <i><strong>concavity down</strong></i>`)
-    sb.innerHTML=(`b is negative and a is negative, the vertex will be on quadrant 2 and the graph will cross the axie y <i><strong>decreasing</strong></i>`)
-    sc.innerHTML=(`c is positive, the graph will cross the y axis <i><strong>above</strong></i> the point 0`)
-    xv.innerHTML=(`The value of Xv is <i><strong>${Xv}</strong></i>`)
-    yv.innerHTML=(`The value of Yv is <i><strong>${Yv}</strong></i>`)
-    i.innerHTML = ('<img src="../static/imagens/a negative, b negative, c positive .png" alt="graph">')
-    expression.innerHTML = (`Your quadratic function will look like: <i>${a}x² ${b}x + ${c}</i>`)
+        // a negative, b positive, c negative
+    } else if(a < 0 && b > 0 && c < 0) {
+    
+        i.innerHTML = ('<img src="../static/imagens/a negative, b positive, c negative.png" alt="graph">')
 
-    // a negative, b positive, c negative
-}else if(a < 0 && b > 0 && c < 0)
-{
-    sa.innerHTML=(`a is negative, the graph has a <i><strong>concavity down</strong></i>`)
-    sb.innerHTML=(`b is positive and a is negative, the vertex will be on quadrant 1 and the graph will cross the axie y <i><strong>increasing</strong></i>`)
-    sc.innerHTML=(`c is negative, the graph will cross the y axis <i><strong>below</strong></i> the point 0`)
-    xv.innerHTML=(`The value of Xv is <i><strong> ${Xv}</strong></i>`)
-    yv.innerHTML=(`The value of Yv is <i><strong>${Yv}</strong></i>`)
-    i.innerHTML = ('<img src="../static/imagens/a negative, b positive, c negative.png" alt="graph">')
-    expression.innerHTML = (`Your quadratic function will look like: <i>${a}x² + ${b}x ${c}</i>`)
+        // a negative, b negative, c negative 
+    } else if(a < 0 && b < 0 && c < 0) {
+    
+        i.innerHTML = ('<img src="../static/imagens/a negative, b negative, c negative.png" alt="graph">')
 
-    // a negative, b negative, c negative 
-}else if(a < 0 && b < 0 && c < 0)
-{
-    sa.innerHTML=(`a is negative, the graph has a <i><strong>concavity down</strong></i>`)
-    sb.innerHTML=(`b is negative and a is negative, the vertex will be on quadrant 2 and the graph will cross the axie y <i><strong>decreasing</strong></i>`)
-    sc.innerHTML=(`c is negative, the graph will cross the y axis <i><strong>below</strong></i> the point 0`)
-    xv.innerHTML=(`The value of Xv is <i><strong> ${Xv}</strong></i>`)
-    yv.innerHTML=(`The value of Yv is <i><strong>${Yv}</strong></i>`)
-    i.innerHTML = ('<img src="../static/imagens/a negative, b negative, c negative.png" alt="graph">')
-    expression.innerHTML = (`Your quadratic function will look like: <i>${a}x² ${b}x ${c}</i>`)
-} 
+    } 
+
 }
